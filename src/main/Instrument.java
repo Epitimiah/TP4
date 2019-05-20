721f2adc1a5f0f5b7fe1746c64b7244fdad400ef
Lea <epitimiah@users.noreply.github.com>
2019-05-20 18:16:29 +0200

    Initial commit

---
+++ src/main/Instrument.java
@@ -0,0 +1,16 @@
+package main;
+
+import java.util.*;
+/**
+ *
+ * @author 
+ */
+public class Instrument extends Fonds{
+    private ArrayList<Fonds> fonds;
+    
+    public ArrayList<Fonds> getFonds(){
+        return fonds;
+    }
+    
+    public void Instrument(){}
+}
---
+++ src/main/Portefeuille.java
@@ -0,0 +1,24 @@
+package main;
+
+import java.util.*;
+/**
+ *
+ * @author lelel
+ */
+public class Portefeuille {
+    private HashMap<String, Fonds> fondsMap;
+    private HashMap<String, Instrument> instrumentsMap;
+    
+    public HashMap<String, Fonds> getFondsMap(){
+        return fondsMap;
+    }
+    
+    public HashMap<String, Instrument> getInstruMap(){
+        return instrumentsMap;
+    }
+    
+    public void Portefeuille(){
+        getFondsMap();
+        getInstruMap();
+    }
+}
